465fb6369d4033aeba035cd6aae5a5c1007bb362
Benjamin Patterson <Benjamin.Patterson@avalara.com>
2019-11-25 16:32:05 -0800

    built

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-/**
- * Avalara © 2017
- * file: index.js
- */
+'use strict';
 
-import client from './lib/AvaTaxClient';
-module.exports = client;
+var _AvaTaxClient = require('./lib/AvaTaxClient');
 
+var _AvaTaxClient2 = _interopRequireDefault(_AvaTaxClient);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = _AvaTaxClient2.default; /**
+                                          * Avalara © 2017
+                                          * file: index.js
+                                          */
